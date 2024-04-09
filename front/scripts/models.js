52838c2bb52caf283f434aa47c98285d3f76939e
CTunjano <caratungu@gmail.com>
2024-04-08 22:53:14 -0500

    Homework 01 - M2

---
+++ front/scripts/models.js
@@ -0,0 +1,38 @@
+class Movie {
+    constructor (id, title, year, director, duration, genre, rate, poster) {
+        this.id = id;
+        this.title = title;
+        this.year = year;
+        this.director = director;
+        this.duration = duration;
+        this.genre = genre;
+        this.rate = rate;
+        this.poster = poster;
+    };
+};
+
+class Repository {
+    constructor () {
+        this.movies = [];
+        this.id = 0;
+    };
+
+    createMovie (title, year, director, duration, genre, rate, poster) {
+        this.id++;
+        let movie = new Movie (this.id, title, year, director, duration, genre, rate, poster);
+        this.movies.push(movie);
+    };
+
+    getAllMovies () {
+        return this.movies;
+    };
+
+    deleteMovie (id) {
+        this.movies.forEach((movie) => {
+            if (movie.id = id) {
+                let index = this.movies.indexOf(movie);
+                this.movies.splice(index,1);
+            };
+        });
+    };
+};
